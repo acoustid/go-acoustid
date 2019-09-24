bca1f694c898d455bef1636a9acd83356d5281fd
Lukáš Lalinský <lalinsky@gmail.com>
2019-09-24 11:27:20 +0200

    Dockerfile for aindex

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
-go build ./index
-go build ./index/cmd/aindex
+targets="linux/amd64"
+
+for target in $targets
+do
+    os="$(echo $target | cut -d '/' -f1)"
+    arch="$(echo $target | cut -d '/' -f2)"
+    GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o aindex-$os-$arch ./index/cmd/aindex
+done
