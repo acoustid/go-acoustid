ad553b87d1cd4c717590630076e5353f03f6ef0c
Lukáš Lalinský <lalinsky@gmail.com>
2019-09-29 16:35:34 +0200

    Make aindex executable

--- ci/build-images.sh
+++ ci/build-images.sh
@@ -4,5 +4,7 @@ set -eux
 
 VERSION=$(echo "$GITHUB_REF" | cut -d/ -f3-)
 
+chmod +x aindex-linux-amd64
+
 docker build . -f Dockerfile.index -t quay.io/acoustid/acoustid-index:$VERSION
 docker build . -f Dockerfile.index-updater --build-arg VERSION=$VERSION -t quay.io/acoustid/acoustid-index-updater:$VERSION
