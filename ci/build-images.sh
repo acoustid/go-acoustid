c7e3c1c1949e2b75e5d67e131a29564c62ddb3fd
Lukáš Lalinský <lalinsky@gmail.com>
2020-12-01 17:41:57 +0100

    Build server docker image

--- ci/build-images.sh
+++ ci/build-images.sh
@@ -9,3 +9,6 @@ chmod +x dist/*
 docker build . -f Dockerfile.index -t quay.io/acoustid/acoustid-index:$VERSION
 docker build . -f Dockerfile.index-updater --build-arg VERSION=$VERSION -t quay.io/acoustid/acoustid-index-updater:$VERSION
 docker build . -f Dockerfile.index-proxy --build-arg VERSION=$VERSION -t quay.io/acoustid/acoustid-index-proxy:$VERSION
+
+docker build . -f Dockerfile.server -t quay.io/acoustid/acoustid-server:$VERSION
+docker build . -f Dockerfile.server-api --build-arg VERSION=$VERSION -t quay.io/acoustid/acoustid-server-api:$VERSION
--- ci/push-images.sh
+++ ci/push-images.sh
@@ -11,3 +11,6 @@ VERSION=$(echo "$GITHUB_REF" | cut -d/ -f3-)
 # docker push quay.io/acoustid/acoustid-index:$VERSION
 docker push quay.io/acoustid/acoustid-index-updater:$VERSION
 docker push quay.io/acoustid/acoustid-index-proxy:$VERSION
+
+# docker push quay.io/acoustid/acoustid-server:$VERSION
+# docker push quay.io/acoustid/acoustid-server-api:$VERSION
