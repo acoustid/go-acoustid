a4150254b4e04328cf2258d603352ce91f670a13
Lukáš Lalinský <lalinsky@gmail.com>
2021-11-27 21:31:11 +0100

    Fix path to wait-for-it.sh

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-./dev/wait-for-it.sh $ACOUSTID_TEST_REDIS_HOST:$ACOUSTID_TEST_REDIS_PORT
-./dev/wait-for-it.sh $ACOUSTID_TEST_POSTGRESQL_HOST:$ACOUSTID_TEST_POSTGRESQL_PORT
+./scripts/dev/wait-for-it.sh $ACOUSTID_TEST_REDIS_HOST:$ACOUSTID_TEST_REDIS_PORT
+./scripts/dev/wait-for-it.sh $ACOUSTID_TEST_POSTGRESQL_HOST:$ACOUSTID_TEST_POSTGRESQL_PORT
 
 go test -v -covermode=count -coverprofile=coverage.out ./...
