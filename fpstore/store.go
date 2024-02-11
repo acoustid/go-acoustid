4cd83150b15467ef91dd393b5478ab392ff3dcc8
Lukas Lalinsky <llalinsky@live365.com>
2024-02-11 20:53:29 +0100

    Avoid duplication

--- fpstore/store.go
+++ fpstore/store.go
@@ -3,14 +3,11 @@ package fpstore
 import (
 	"context"
 	"database/sql"
-	"database/sql/driver"
 	"errors"
-	"fmt"
-	"strconv"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/acoustid/go-acoustid/database/fingerprint_db"
 	"github.com/acoustid/go-acoustid/index"
 	pb "github.com/acoustid/go-acoustid/proto/fpstore"
 )
@@ -19,54 +16,6 @@ type FingerprintStore interface {
 	Insert(ctx context.Context, fp *pb.Fingerprint) (uint64, error)
 	Delete(ctx context.Context, id uint64) error
 	Get(ctx context.Context, id uint64) (*pb.Fingerprint, error)
-}
-
-type Uint32Array []uint32
-
-func (a Uint32Array) Value() (driver.Value, error) {
-	if a == nil {
-		return nil, nil
-	}
-	var builder strings.Builder
-	builder.WriteRune('{')
-	for i, item := range a {
-		if i > 0 {
-			builder.WriteRune(',')
-		}
-		builder.WriteString(strconv.FormatInt(int64(int32(item)), 10))
-	}
-	builder.WriteRune('}')
-	return builder.String(), nil
-}
-
-func (a *Uint32Array) Scan(src interface{}) error {
-	switch src := src.(type) {
-	case []byte:
-		return a.scanString(string(src))
-	case string:
-		return a.scanString(src)
-	case nil:
-		*a = nil
-		return nil
-	}
-	return fmt.Errorf("cannot convert %T to Uint32Array", src)
-}
-
-func (a *Uint32Array) scanString(src string) error {
-	if strings.HasPrefix(src, "{") && strings.HasSuffix(src, "}") {
-		src = strings.Trim(src, "{}")
-	}
-	items := strings.Split(src, ",")
-	result := make([]uint32, len(items))
-	for i, item := range items {
-		value, err := strconv.ParseInt(item, 10, 32)
-		if err != nil {
-			return err
-		}
-		result[i] = uint32(int32(value))
-	}
-	*a = result
-	return nil
 }
 
 type PostgresFingerprintStore struct {
@@ -115,7 +64,7 @@ func (s *PostgresFingerprintStore) checkV1(ctx context.Context, id uint64) (bool
 }
 
 func (s *PostgresFingerprintStore) getV1(ctx context.Context, id uint64) (*pb.Fingerprint, error) {
-	var hashes Uint32Array
+	var hashes fingerprint_db.Uint32Array
 	query := "SELECT fingerprint FROM fingerprint WHERE id = $1"
 	err := s.db.QueryRowContext(ctx, query, id).Scan(&hashes)
 	if err != nil {
@@ -141,17 +90,10 @@ func (s *PostgresFingerprintStore) getV2(ctx context.Context, id uint64) (*pb.Fi
 }
 
 func (s *PostgresFingerprintStore) Get(ctx context.Context, id uint64) (*pb.Fingerprint, error) {
-	fp, err := s.getV2(ctx, id)
+	fp, err := s.getV1(ctx, id)
 	if err != nil {
-		log.Warnf("failed to get fingerprint from v2 table: %v", err)
+		log.Warnf("failed to get fingerprint from v1 table: %v", err)
 		return nil, err
-	}
-	if fp == nil {
-		fp, err = s.getV1(ctx, id)
-		if err != nil {
-			log.Warnf("failed to get fingerprint from v1 table: %v", err)
-			return nil, err
-		}
 	}
 	return fp, nil
 }
