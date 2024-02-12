b4def72f36cb2ea1cdbea744b9f33c805671dc0e
Lukas Lalinsky <llalinsky@live365.com>
2024-02-12 21:36:22 +0100

    Fix tests

--- pkg/chromaprint/match_test.go
+++ pkg/chromaprint/match_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func loadTestFingerprint(t *testing.T, name string) *common_pb.Fingerprint {
-	data, err := os.ReadFile(path.Join("..", "testdata", name+".txt"))
+	data, err := os.ReadFile(path.Join("..", "..", "testdata", name+".txt"))
 	require.NoError(t, err)
 	fp, err := ParseFingerprintString(string(data))
 	require.NoError(t, err)
